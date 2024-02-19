4a5aad6eda6bd2f7ea7ae452a73ce5fb61054133
Kauan Dias <kauandias772@gmail.com>
2024-02-19 10:13:35 -0300

    Added add imc function

--- Imc/Program.cs
+++ Imc/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Imc;
+using Cloudcrate.AspNetCore.Blazor.Browser.Storage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddStorage();
+
 
 await builder.Build().RunAsync();
